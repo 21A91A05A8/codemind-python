8b86bd44264b41f25334eeff672effd61c6c9dd0
21A91A05A8 <97424469+21A91A05A8@users.noreply.github.com>
2022-04-30 21:09:53 +0530

    n=int(input()) k=n i=k*k rev=0 jrev=0 while (n!=0):     d=n%10     rev=(rev*10)+d     n=n//10 j=rev*rev while (j!=0):     e=j%10     jrev=(jrev*10)+e     j=j//10 if jrev==i:     print("True") else:     print("False");

---
+++ Adam_number.py
@@ -0,0 +1,18 @@
+n=int(input())
+k=n
+i=k*k
+rev=0
+jrev=0
+while (n!=0):
+    d=n%10
+    rev=(rev*10)+d
+    n=n//10
+j=rev*rev 
+while (j!=0):
+    e=j%10
+    jrev=(jrev*10)+e
+    j=j//10
+if jrev==i:
+    print("True")
+else:
+    print("False");
