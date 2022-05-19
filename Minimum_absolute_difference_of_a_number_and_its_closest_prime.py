5c7031c09e5da55c26d89ec1d8c154224fd1872e
21A91A05A8 <97424469+21A91A05A8@users.noreply.github.com>
2022-05-19 10:59:20 +0530

    from math import sqrt
    # Function to check if a number is
    # prime or not
    def isPrime(N):
        k = int(sqrt(N)) + 1
        for i in range(2, k, 1):
            if (N % i == 0):
                return False
             
        return True
     
    # Function to find the minimum absolute
    # difference between a number and its
    # closest prime
    def getDifference(N):
        if (N == 0):
            return 2
        elif (N == 1):
            return 1
        elif (isPrime(N)):
            return 0
     
        # Variables to store first prime
        # above and below N
        aboveN = -1
        belowN = -1
             
        # Finding first prime number
        # greater than N
        n1 = N + 1
        while (True):
            if (isPrime(n1)):
                aboveN = n1
                break
                 
            else:
                n1 += 1
     
        # Finding first prime number
        # less than N
        n1 = N - 1
        while (True):
            if (isPrime(n1)):
                belowN = n1
                break
                 
            else:
                n1 -= 1
     
        # Variables to store the differences
        diff1 = aboveN - N
        diff2 = N - belowN
     
        return min(diff1, diff2)
         
    # Driver code
    if __name__ == '__main__':
        N =int(input())
        print(getDifference(N))

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -1,21 +1,60 @@
-a=int(input())#perivious prime
-for n in range(a,1,-1):
-    for i in range(2,int(n**0.5)+1):
-        if n%i==0:
+from math import sqrt
+ 
+# Function to check if a number is
+# prime or not
+def isPrime(N):
+    k = int(sqrt(N)) + 1
+    for i in range(2, k, 1):
+        if (N % i == 0):
+            return False
+         
+    return True
+ 
+# Function to find the minimum absolute
+# difference between a number and its
+# closest prime
+def getDifference(N):
+    if (N == 0):
+        return 2
+    elif (N == 1):
+        return 1
+    elif (isPrime(N)):
+        return 0
+ 
+    # Variables to store first prime
+    # above and below N
+    aboveN = -1
+    belowN = -1
+         
+    # Finding first prime number
+    # greater than N
+    n1 = N + 1
+    while (True):
+        if (isPrime(n1)):
+            aboveN = n1
             break
-    else:
-        #print(n)
-        break
-b=a
-while True:
-       for i in range(2,int(a**0.5)+1):#next prime
-          if a%i==0:
+             
+        else:
+            n1 += 1
+ 
+    # Finding first prime number
+    # less than N
+    n1 = N - 1
+    while (True):
+        if (isPrime(n1)):
+            belowN = n1
             break
-       else:
-          #print(a)
-          break
-       a+=1
-if abs(b-n)>=abs(a-b):
-    print(abs(a-b))
-else:
-    print(abs(b-n))
+             
+        else:
+            n1 -= 1
+ 
+    # Variables to store the differences
+    diff1 = aboveN - N
+    diff2 = N - belowN
+ 
+    return min(diff1, diff2)
+     
+# Driver code
+if __name__ == '__main__':
+    N =int(input())
+    print(getDifference(N))
