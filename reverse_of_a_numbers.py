bc845f62a01aba5e1618212815c78f03e5de013d
21A91A05A8 <97424469+21A91A05A8@users.noreply.github.com>
2022-06-29 21:38:15 +0530

    def reverseNumber(number):         x = 0         n = abs(number)         rev = 0         while(n > 0):             a = n % 10             rev = rev * 10 + a             n = n // 10         if(number < 0):             return ("-" + str(rev) )         return (rev) number=int(input()) print(reverseNumber(number))

--- reverse_of_a_numbers.py
+++ reverse_of_a_numbers.py
@@ -1,7 +1,13 @@
-n=int(input())
-rev=0
-while n>0:
-    d=n%10
-    n=n//10
-    rev=rev*10+d
-print(rev)    
+def reverseNumber(number):
+        x = 0
+        n = abs(number)
+        rev = 0
+        while(n > 0):
+            a = n % 10
+            rev = rev * 10 + a
+            n = n // 10
+        if(number < 0):
+            return ("-" + str(rev) )
+        return (rev)
+number=int(input())
+print(reverseNumber(number))
