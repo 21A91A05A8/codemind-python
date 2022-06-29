5bdf9ddf9c4f906f3e20c62132faaba80c599db3
21A91A05A8 <97424469+21A91A05A8@users.noreply.github.com>
2022-06-29 21:31:38 +0530

    num = int(input()) num_of_digits = len(str(num)) square = num**2 last_digits = square%pow(10,num_of_digits) if last_digits == num:   print('Automorphic Number') else:   print('Not an Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -5,4 +5,4 @@ last_digits = square%pow(10,num_of_digits)
 if last_digits == num:  
   print('Automorphic Number')  
 else:  
-  print('Not an Automorphic Number')  
+  print('Not an Automorphic Number')
