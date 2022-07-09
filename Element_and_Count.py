3b0a93e1c5c5d71e434eeb32c06dbe17c63e6437
21A91A05A8 <97424469+21A91A05A8@users.noreply.github.com>
2022-07-09 15:53:23 +0530

    n=int(input()) a=list(map(int,input().split())) l=[] r=[] for i in a:     if i not in l:         l.append(i) for i in range(0,len(l)):     c=a.count(l[i])     r.append(l[i])     r.append(c) print(*r)

--- Element_and_Count.py
+++ Element_and_Count.py
@@ -1,11 +1,12 @@
-n=int(input())  
-c=1
-a = list(map(int,input().split())) #list
-for i in range(0,n):
-    c=1
-    if a[i]>0:
-        for j in range(i+1,n):
-            if a[i]==a[j]:
-                c+=1
-                a[j]=-1  
-        print(a[i],c,end=' ')
+n=int(input())
+a=list(map(int,input().split()))
+l=[]
+r=[]
+for i in a:
+    if i not in l:
+        l.append(i)
+for i in range(0,len(l)):
+    c=a.count(l[i])
+    r.append(l[i])
+    r.append(c)
+print(*r)
