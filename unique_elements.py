a7191821f592757eb9b37b2db5ad73aea3bfa636
21A91A05A8 <97424469+21A91A05A8@users.noreply.github.com>
2022-07-14 06:16:47 +0530

    n=int(input()) a=list(map(int,input().split())) temp=[] sum=0 for element in a:     if(element not in temp):         temp.append(element) print(*temp)

--- unique_elements.py
+++ unique_elements.py
@@ -2,7 +2,7 @@ n=int(input())
 a=list(map(int,input().split()))
 temp=[]
 sum=0
-for element in a: #elmt there in a
-    if(element not in temp):  #not in
+for element in a:
+    if(element not in temp):
         temp.append(element) 
 print(*temp)
