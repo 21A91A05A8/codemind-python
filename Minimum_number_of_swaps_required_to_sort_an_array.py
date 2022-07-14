b9287063d9b40cbb1696f64cd04fdd2ca807eb1a
21A91A05A8 <97424469+21A91A05A8@users.noreply.github.com>
2022-07-14 06:06:10 +0530

    from functools import cmp_to_key
    def cmp(a, b):
        return a - b
     
    def minSwaps(nums):
        Len = len(nums)
        map = {}
        for i in range(Len):
            map[nums[i]] = i
     
        nums = sorted(nums, key = cmp_to_key(cmp))
     
        # To keep track of visited elements. Initialize
        # all elements as not visited or false.
        visited = [False for col in range(Len)]
         
        # Initialize result
        ans = 0
        for i in range(Len):
     
            # already swapped and corrected or
            # already present at correct pos
            if (visited[i] or map[nums[i]] == i):
                continue
     
            j,cycle_size = i, 0
            while (visited[j] == False):
                visited[j] = True
     
                # move to next node
                j = map[nums[j]]
                cycle_size += 1
     
            # Update answer by adding current cycle.
            if (cycle_size > 0):
                ans += (cycle_size - 1)
     
        return ans
     
    n=int(input())
    a = list(map(int,input().split()))
    print(minSwaps(a))

---
+++ Minimum_number_of_swaps_required_to_sort_an_array.py
@@ -0,0 +1,43 @@
+from functools import cmp_to_key
+ 
+def cmp(a, b):
+    return a - b
+ 
+def minSwaps(nums):
+    Len = len(nums)
+    map = {}
+    for i in range(Len):
+        map[nums[i]] = i
+ 
+    nums = sorted(nums, key = cmp_to_key(cmp))
+ 
+    # To keep track of visited elements. Initialize
+    # all elements as not visited or false.
+    visited = [False for col in range(Len)]
+     
+    # Initialize result
+    ans = 0
+    for i in range(Len):
+ 
+        # already swapped and corrected or
+        # already present at correct pos
+        if (visited[i] or map[nums[i]] == i):
+            continue
+ 
+        j,cycle_size = i, 0
+        while (visited[j] == False):
+            visited[j] = True
+ 
+            # move to next node
+            j = map[nums[j]]
+            cycle_size += 1
+ 
+        # Update answer by adding current cycle.
+        if (cycle_size > 0):
+            ans += (cycle_size - 1)
+ 
+    return ans
+ 
+n=int(input())
+a = list(map(int,input().split()))
+print(minSwaps(a))
