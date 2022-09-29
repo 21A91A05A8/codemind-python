06eb628b07291bed296fde4053858e96932071da
21A91A05A8 <97424469+21A91A05A8@users.noreply.github.com>
2022-09-29 18:00:25 +0530

    def count_Triplets(A, N):     count = 0     A.sort()     for i in range(N):         for j in range(i + 1, N):             for k in range(j + 1, N):                 if A[i] + A[j] == A[k]:                     count = count + 1     if(count==0):         return "-1"
        return count
     
    t=int(input())
    while(t):
        n=int(input())
        l=list(map(int,input().split()))
        print(count_Triplets(l,n))
        t-=1

---
+++ Count_the_triplets.py
@@ -0,0 +1,19 @@
+def count_Triplets(A, N):
+    count = 0
+    A.sort()
+    for i in range(N):
+        for j in range(i + 1, N):
+            for k in range(j + 1, N):
+                if A[i] + A[j] == A[k]:
+                    count = count + 1
+    if(count==0):
+        return "-1"
+ 
+    return count
+ 
+t=int(input())
+while(t):
+    n=int(input())
+    l=list(map(int,input().split()))
+    print(count_Triplets(l,n))
+    t-=1
